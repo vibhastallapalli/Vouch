874940cc5e8972a35128524ff88c2acd03fa7320
Vibhas Tallapalli <vtallapa@uwaterloo.ca>
2026-09-22 15:54:41 -0400

    Clarify wallet disconnect action

--- app/src/ds/WalletChip.jsx
+++ app/src/ds/WalletChip.jsx
@@ -1,7 +1,7 @@
 export default function WalletChip({ connected = false, address = 'addr1···x7q9', busy = false, onClick }) {
   if (connected)
     return (
-      <button type="button" className="mn-chip mn-chip--on" onClick={onClick} title="Wallet connected. Click to disconnect.">
+      <button type="button" className="mn-chip mn-chip--on" onClick={onClick} title="Wallet connected. Click to disconnect." aria-label={'Disconnect wallet ' + address}>
         <span className="mn-chip__mark">Lace</span>
         <span>{address}</span>
       </button>
